597dca0186cacde1c0b13b4f31712fbe6c2925de
kunal yadav <kunalyadav1601@gmail.com>
2023-09-15 17:54:12 +0530

    models made

---
+++ server/models/Advocate.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const AdvocateSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+        },
+
+        lastName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+        },
+        phoneNo: {
+            type: Number,
+            require: true,
+            max: 9999999999,
+        },
+        email: {
+            type: String,
+            // require: true ,
+            unique: true,
+        },
+        password: {
+            type: String,
+            require: true,
+            min: 3
+        },
+        picturePath: {
+            type: String,
+            default: "",
+          },
+        gender: {
+            type:String,
+        },
+        age:{
+            type: Number,
+        },
+        location:{
+            type: String,
+        },
+        barCode:{
+            type: String,
+            require: true,
+        },
+        description: {
+            type: String,
+            require: true,
+        },
+        experience: String,
+        college: {
+            type: String,
+            require: true,
+        }
+        
+    },
+    { timestamps: true }
+);
+
+const Advocate = mongoose.model("advocate", AdvocateSchema);
+export default Advocate;
---
+++ server/models/Client.js
@@ -0,0 +1,52 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const ClientSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+        },
+
+        lastName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+        },
+        phoneNo: {
+            type: Number,
+            require: true,
+            max: 9999999999,
+        },
+        email: {
+            type: String,
+            // require: true ,
+            unique: true,
+        },
+        password: {
+            type: String,
+            require: true,
+            min: 3
+        },
+        picturePath: {
+            type: String,
+            default: "",
+          },
+        gender: {
+            type:String,
+        },
+        age:{
+            type: Number,
+        },
+        location:{
+            type: String,
+        },
+        
+    },
+    { timestamps: true }
+);
+
+const Client = Mongoose.model("client", ClientSchema);
+export default Client;
